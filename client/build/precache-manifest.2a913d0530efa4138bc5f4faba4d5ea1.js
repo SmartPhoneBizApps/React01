bee695db5d54948be9bd128c080edccf48dcd689
atul_gup@yahoo.co.uk <atul_gup@yahoo.co.uk>
2020-04-07 20:03:58 +0100

    make it better

---
+++ client/build/precache-manifest.2a913d0530efa4138bc5f4faba4d5ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330d394868c1398d1aefbdda2d4bb699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6a3980f7437f5933bda",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "4365715db8d0b40fabef",
+    "url": "/static/js/2.ed618c0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed618c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a3980f7437f5933bda",
+    "url": "/static/js/main.2da8f9f0.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
